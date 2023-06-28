b020a131811a16a7f9971d72250b56e8db20926c
Axel-Aranibar <57770054+AxelArani-code@users.noreply.github.com>
2023-06-28 09:20:46 -0300

    2
    2

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const workData = [
+  {
+    name: 'Copy Pinterest',
+    description:
+        "When I learned the basics of app development I didn't have a clue. So I came up with the idea of making a Pinterest replica to see how I could use my knowledge and learn more about java and Android architecture.",
+    ImgUrl: 'work_1.png',
+    tech: ['Java', 'Firebase', 'Retrofit'],
+    demoUrl: 'https://github.com/AxelArani-code',
+    gitUrl: 'https://github.com/AxelArani-code/Proyect-Pinteres-Java',
+  },
+  {
+    name: 'App Restaurant Bar ',
+    description:
+        'I generated an app for a restaurant bar where it was basically a menu where the prices of all the products and the daily specials appeared. It also had an interaction with the owner who could evaluate how many customers used the app.',
+    ImgUrl: 'work_2.png',
+    tech: ['Java', 'Firebase', 'Kotlin', 'Retrofit'],
+    demoUrl: 'https://github.com/AxelArani-code',
+    gitUrl: 'https://github.com/AxelArani-code/El-Patio-Bar',
+  },
+  {
+    name: 'Mayah',
+    description:
+        'Mayah is one of my first freelance jobs. Where I implemented new features to the app (like getting the data from an api, where I multiplied the products and then when you had the total of your order generated a subtotal), fix some bugs, among other things.',
+    ImgUrl: 'work_3.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://mayah.co/',
+    gitUrl: 'https://github.com/AxelArani-code',
+  },
+  {
+    name: 'Web Films',
+    description:
+        'Web Films is one of my web, mysql, php projects. In my career as a software developer I was asked to do a project where I could apply my skills as a developer. Basically the web gives you the top movies of the year.',
+    ImgUrl: 'work_4.png',
+    tech: ['HTML', 'css', 'javascript', 'PHP', 'MYSQL'],
+    demoUrl: 'https://github.com/AxelArani-code',
+    gitUrl: 'https://github.com/AxelArani-code/Proyect-Flims',
+  },
+  {
+    name: 'Design UX',
+    description:
+        'Make some designs where to improve the user experience of each brand and design. I used tools like Figma and Photoshop. I have more examples on my Instagram',
+    ImgUrl: 'work_5.png',
+    tech: ['Photoshop', 'Figma'],
+    demoUrl: 'https://www.instagram.com/alexdesign65/?igshid=MzRlODBiNWFlZA%3D%3D',
+    gitUrl: 'https://www.instagram.com/alexdesign65/?igshid=MzRlODBiNWFlZA%3D%3D',
+  }
+];
+
+export default workData;
